9c567ed7d33401fccccbe1edf20bee26a111f87e
Omar Rafael Guerra Alvarez <agrom1978@yahoo.com>
2026-09-14 17:30:56 -0400

    Create emotion detection function

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,32 @@
+"""Emotion detection module using Watson NLP."""
+
+import requests
+
+
+def emotion_detector(text_to_analyze):
+    """Send text to Watson NLP and return the emotion analysis response."""
+
+    url = (
+        "https://sn-watson-emotion.labs.skills.network/v1/"
+        "watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    )
+
+    headers = {
+        "grpc-metadata-mm-model-id":
+        "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(
+        url=url,
+        headers=headers,
+        json=input_json,
+        timeout=10
+    )
+
+    return response.text
